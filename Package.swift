373b26153de31dd0dcc39134aa88555535a77fae
runner <runner@Mac-1751874050544.local>
2025-07-07 08:18:07 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "umbrella",
-      url: "https://github.com/HedvigInsurance/umbrella/releases/download/0.0.20250707071714/umbrella.xcframework.zip",
-      checksum: "a6b770d655b76d0c7bb4b12031b6f55c9a379e7dc0b955a359c291cf6bca380c"
+      url: "https://github.com/HedvigInsurance/umbrella/releases/download/0.0.20250707081758/umbrella.xcframework.zip",
+      checksum: "d52cd7617c947d2eabb1b9e5bdca2a2c29cb966253674291ef8f5e8815a1d4af"
     )
   ]
 )
