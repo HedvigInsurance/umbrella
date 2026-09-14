c2491844b1c8e254bbc52c4c4660ad66f7f27d19
Anka <runner@chi22-dq252-0996655a-0bc7-4bdd-a712-f1904d3c7cb0-16A43E7D6F12.local>
2026-09-14 13:06:30 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "HedvigShared",
-      url: "https://github.com/HedvigInsurance/umbrella/releases/download/0.0.20260914090431/HedvigShared.xcframework.zip",
-      checksum: "5834ae879c03cf4e94688c410cf7e9219c7dbe4be2f038f3b5eb82a0497b8f34"
+      url: "https://github.com/HedvigInsurance/umbrella/releases/download/0.0.20260914130613/HedvigShared.xcframework.zip",
+      checksum: "63aea23a87bd54e9a97cf83c2406bf5527d6f9598ef4623c33ecebaf3e645fc5"
     )
   ]
 )
